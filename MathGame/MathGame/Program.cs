ab065963825968fec277148c10e5f4c9f8f7ee8d
AlexRamos_ <arv_lad@hotmail.com>
2026-03-26 12:13:06 -0600

    Monolithic - Update

--- MathGame/MathGame/Program.cs
+++ MathGame/MathGame/Program.cs
@@ -1,27 +1,44 @@
-﻿
+﻿// Math Game Project
+
+using System.Diagnostics;
+
 string? userInput;
+
 int questionsAsked = 0;
 int maxQuestionAsked = 5;
 int correctAnswers = 0;
+
+Stopwatch stopwatch = new();
+
 List<string> gamesPlayed = [];
-int userResult;
-int numberOne;
-int numberTwo;
-int result;
+
+bool isChronometer = false;
+int difficulty = 10;
+int userResult = 0;
+int numberOne = 0;
+int numberTwo = 0;
+int result = 0;
+
 
 do
 {
     Console.Clear();
 
-    Console.WriteLine("Hello, Welcome to Math Game!\n");
+    Console.WriteLine("Welcome to Math Game!\n");
 
     Console.WriteLine("- 1 - Addition operation.");
-    Console.WriteLine("- 2 - Addition subtraction.");
-    Console.WriteLine("- 3 - Addition division.");
-    Console.WriteLine("- 4 - Addition multiplication.");
-    Console.WriteLine("- 5 - Review results.\n");
+    Console.WriteLine("- 2 - Subtraction operation.");
+    Console.WriteLine("- 3 - Division operation.");
+    Console.WriteLine("- 4 - Multiplication operation.");
+    Console.WriteLine("- 5 - Random Game.");
+    Console.WriteLine("- 6 - Change the difficulty.");
+    Console.WriteLine("- 7 - Review results.");
 
-    Console.WriteLine("Please type a number to choose an option from the menu: ");
+    DisplayDifficulty();
+
+    CheckChronometerStatus();
+
+    Console.WriteLine("Please type a number to choose an option from the menu: (exit)");
 
     userInput = Console.ReadLine().Trim();
 
@@ -32,43 +49,7 @@ do
             {
                 break;
             }
-
-            numberOne = GetRandomNumber(10);
-            numberTwo = GetRandomNumber(10);
-
-            result = numberOne + numberTwo;
-
-            Console.WriteLine("\nWhat is the result of the following operation?");
-            Console.WriteLine($"\n{numberOne} + {numberTwo} = ? \n");
-            userInput = Console.ReadLine().Trim();
-
-            try
-            {
-                userResult = Convert.ToInt32(userInput);
-            }
-            catch
-            {
-                Console.WriteLine("\nInvalid input!");
-                Console.WriteLine("\nPress the Enter key to continue.");
-                Console.ReadLine();
-                break;
-            }
-
-            if (userResult == result)
-            {
-                Console.WriteLine("\nCorrect!");
-                questionsAsked++;
-                correctAnswers++;
-            }
-            else
-            {
-                Console.WriteLine("\nWrong answer!");
-                questionsAsked++;
-                correctAnswers--;
-            }
-
-            Console.WriteLine("\nPress the Enter key to continue.");
-            Console.ReadLine();
+            SumOperation();
             break;
 
         case "2":
@@ -76,43 +57,7 @@ do
             {
                 break;
             }
-
-            numberOne = GetRandomNumber(10);
-            numberTwo = GetRandomNumber(10);
-
-            result = numberOne - numberTwo;
-
-            Console.WriteLine("\nWhat is the result of the following operation?");
-            Console.WriteLine($"\n{numberOne} - {numberTwo} = ? \n");
-            userInput = Console.ReadLine().Trim();
-
-            try
-            {
-                userResult = Convert.ToInt32(userInput);
-            }
-            catch
-            {
-                Console.WriteLine("\nInvalid input!");
-                Console.WriteLine("\nPress the Enter key to continue.");
-                Console.ReadLine();
-                break;
-            }
-
-            if (userResult == Math.Abs(result))
-            {
-                Console.WriteLine("\nCorrect!");
-                questionsAsked++;
-                correctAnswers++;
-            }
-            else
-            {
-                Console.WriteLine("\nWrong answer!");
-                questionsAsked++;
-                correctAnswers--;
-            }
-
-            Console.WriteLine("\nPress the Enter key to continue.");
-            Console.ReadLine();
+            SubOperation();
             break;
 
         case "3":
@@ -120,61 +65,7 @@ do
             {
                 break;
             }
-
-            numberOne = GetRandomNumber(100);
-            numberTwo = 2;
-
-            result = numberOne % numberTwo;
-
-            if (result == 0)
-            {
-                result = numberOne / numberTwo;
-            }
-            else
-            {
-                while (result != 0)
-                {
-                    numberOne = GetRandomNumber(100);
-
-                    result = numberOne % numberTwo;
-                }
-
-                result = numberOne / numberTwo;
-            }
-
-            Console.WriteLine("\nWhat is the result of the following operation?");
-            Console.WriteLine($"\n{numberOne} / {numberTwo} = ? \n");
-            userInput = Console.ReadLine().Trim();
-
-            try
-            {
-                userResult = Convert.ToInt32(userInput);
-            }
-            catch
-            {
-                Console.WriteLine("\nInvalid input!");
-                Console.WriteLine("\nPress the Enter key to continue.");
-                Console.ReadLine();
-                break;
-            }
-
-            if (userResult == result)
-            {
-                Console.WriteLine("\nCorrect!");
-                questionsAsked++;
-                correctAnswers++;
-            }
-            else
-            {
-                Console.WriteLine("\nWrong answer!");
-                questionsAsked++;
-                correctAnswers--;
-            }
-
-            Console.WriteLine("\nYou have choose division.");
-
-            Console.WriteLine("\nPress the Enter key to continue.");
-            Console.ReadLine();
+            DivOperation();
             break;
 
         case "4":
@@ -182,54 +73,24 @@ do
             {
                 break;
             }
-
-            numberOne = GetRandomNumber(10);
-            numberTwo = GetRandomNumber(10);
-
-            result = numberOne * numberTwo;
-
-            Console.WriteLine("\nWhat is the result of the following operation?");
-            Console.WriteLine($"\n{numberOne} x {numberTwo} = ? \n");
-            userInput = Console.ReadLine().Trim();
-
-            try
-            {
-                userResult = Convert.ToInt32(userInput);
-            }
-            catch
-            {
-                Console.WriteLine("\nInvalid input!");
-                Console.WriteLine("\nPress the Enter key to continue.");
-                Console.ReadLine();
-                break;
-            }
-
-            if (userResult == result)
-            {
-                Console.WriteLine("\nCorrect!");
-                questionsAsked++;
-                correctAnswers++;
-            }
-            else
-            {
-                Console.WriteLine("\nWrong answer!");
-                questionsAsked++;
-                correctAnswers--;
-            }
-
-            Console.WriteLine("\nYou have choose multiplication.");
-
-            Console.WriteLine("\nPress the Enter key to continue.");
-            Console.ReadLine();
+            MultOperation();
             break;
 
         case "5":
+            PlayRandomGame();
+            break;
+
+        case "6":
+            ModifyDifficulty();
+            break;
+
+        case "7":
 
             Console.WriteLine("\nReview your games!\n");
 
-            if (gamesPlayed.Count() == 0)
+            if (gamesPlayed.Count == 0)
             {
-                Console.WriteLine("You have not complete any game yet!");
+                Console.WriteLine("You haven't finished any games yet!");
                 Console.WriteLine("\nPress the Enter key to continue.");
                 Console.ReadLine();
                 break;
@@ -250,28 +111,272 @@ do
 }
 while (userInput != "exit");
 
+
 int GetRandomNumber(int number)
 {
     Random randomNumber = new();
     return randomNumber.Next(number);
 }
 
+
 bool CheckStatusGame()
 {
     if (questionsAsked == maxQuestionAsked)
     {
-        gamesPlayed.Add($"You got {correctAnswers}/{maxQuestionAsked} correct answers!");
+        TimeSpan timeSpan = stopwatch.Elapsed;
+
+        string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}", timeSpan.Hours, timeSpan.TotalMinutes, timeSpan.Seconds, timeSpan.Milliseconds / 10);
+
+        gamesPlayed.Add($"You got {correctAnswers}/{maxQuestionAsked} correct answers! - Time: {elapsedTime}");
 
         Console.WriteLine("\nGame over!\n");
         Console.WriteLine($"Here are your results: {correctAnswers}/{maxQuestionAsked}");
+        Console.WriteLine($"You complete the game under: {elapsedTime}");
         Console.WriteLine("\nPress the Enter key to play a new game.");
-        Console.ReadLine();
 
         correctAnswers = 0;
         questionsAsked = 0;
+
+        stopwatch.Reset();
+        isChronometer = false;
+
+        Console.ReadLine();
 
         return true;
     }
 
     return false;
+}
+
+
+void CheckQuestion(int userAnswer, int operationResult)
+{
+    if (userAnswer == operationResult)
+    {
+        Console.WriteLine("\nCorrect!");
+        questionsAsked++;
+        correctAnswers++;
+    }
+    else
+    {
+        Console.WriteLine("\nWrong answer!");
+        questionsAsked++;
+    }
+
+    Console.WriteLine("\nPress the Enter key to continue.");
+    Console.ReadLine();
+}
+
+
+int DisplayOperation(char charOperation, int valueOne, int valueTwo)
+{
+    int parsedInput = 0;
+
+    do
+    {
+        Console.Clear();
+        Console.WriteLine("\nWhat is the result of the following operation?");
+        Console.WriteLine($"\n{valueOne} {charOperation} {valueTwo} = ? \n");
+        userInput = Console.ReadLine().Trim();
+    }
+    while (int.TryParse(userInput, out parsedInput) == false);
+
+    return parsedInput;
+}
+
+
+void SumOperation()
+{
+    numberOne = GetRandomNumber(difficulty);
+    numberTwo = GetRandomNumber(difficulty);
+
+    result = numberOne + numberTwo;
+
+    userResult = DisplayOperation('+', numberOne, numberTwo);
+
+    CheckQuestion(userResult, result);
+}
+
+
+void SubOperation()
+{
+    numberOne = GetRandomNumber(difficulty);
+    numberTwo = GetRandomNumber(difficulty);
+
+    result = Math.Abs(numberOne - numberTwo);
+
+    userResult = DisplayOperation('-', numberOne, numberTwo);
+
+    CheckQuestion(userResult, result);
+}
+
+
+void DivOperation()
+{
+    numberOne = GetRandomNumber(difficulty);
+    numberTwo = 2;
+
+    result = numberOne % numberTwo;
+
+    if (result == 0)
+    {
+        result = numberOne / numberTwo;
+    }
+    else
+    {
+        while (result != 0)
+        {
+            numberOne = GetRandomNumber(difficulty);
+
+            result = numberOne % numberTwo;
+        }
+
+        result = numberOne / numberTwo;
+    }
+
+    userResult = DisplayOperation('/', numberOne, numberTwo);
+
+    CheckQuestion(userResult, result);
+}
+
+void MultOperation()
+{
+    numberOne = GetRandomNumber(difficulty);
+    numberTwo = GetRandomNumber(difficulty);
+
+    result = numberOne * numberTwo;
+
+    userResult = DisplayOperation('x', numberOne, numberTwo);
+
+    CheckQuestion(userResult, result);
+}
+
+void PlayRandomGame()
+{
+    do
+    {
+        Console.WriteLine("\nDo you want to play a random game? Y/N");
+        userInput = Console.ReadLine().ToLower().Trim();
+    }
+    while (userInput != "y" && userInput != "n");
+
+    if (userInput.Equals("y"))
+    {
+        correctAnswers = 0;
+        questionsAsked = 0;
+
+        CheckChronometerStatus();
+
+        for (int i = 0; i < maxQuestionAsked; i++)
+        {
+            int index = GetRandomNumber(4);
+
+            switch (index)
+            {
+                case 0:
+                    SumOperation();
+                    break;
+
+                case 1:
+                    SubOperation();
+                    break;
+
+                case 2:
+                    DivOperation();
+                    break;
+
+                case 3:
+                    MultOperation();
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        Console.Clear();
+        CheckStatusGame();
+
+    }
+    else
+    {
+        Console.WriteLine("\nYou cancel the game.");
+        Console.WriteLine("\nPress the Enter key to continue.");
+        Console.ReadLine();
+    }
+}
+
+
+void ModifyDifficulty()
+{
+    Console.Clear();
+
+    Console.WriteLine("\nProgression level\n");
+
+    Console.WriteLine("- 1 - Easy Level");
+    Console.WriteLine("- 2 - Medium Level");
+    Console.WriteLine("- 3 - Hard Level");
+
+    DisplayDifficulty();
+
+    Console.WriteLine("Type \"back\" to go back to main menu");
+
+    do
+    {
+        Console.WriteLine("\nChoose an option from the menu\n");
+        userInput = Console.ReadLine().Trim();
+
+        switch (userInput)
+        {
+
+            case "1":
+                Console.WriteLine("\nEasy level setted!");
+                difficulty = 10;
+                Console.WriteLine("\nPress the Enter key to continue.");
+                Console.ReadLine();
+                return;
+
+            case "2":
+                Console.WriteLine("\nMedium level setted!");
+                difficulty = 100;
+                Console.WriteLine("\nPress the Enter key to continue.");
+                Console.ReadLine();
+                return;
+
+            case "3":
+                Console.WriteLine("\nHard level setted!");
+                difficulty = 1000;
+                Console.WriteLine("\nPress the Enter key to continue.");
+                Console.ReadLine();
+                return;
+        }
+    }
+    while (userInput != "back");
+}
+
+void DisplayDifficulty()
+{
+    switch (difficulty)
+    {
+        case 10:
+            Console.WriteLine($"\nCurrent level of difficulty is Easy.\n");
+            break;
+
+        case 100:
+            Console.WriteLine($"\nCurrent level of difficulty is Medium.\n");
+            break;
+
+        case 1000:
+            Console.WriteLine($"\nCurrent level of difficulty is Hard.\n");
+            break;
+    }
+}
+
+void CheckChronometerStatus()
+{
+    if (!isChronometer)
+    {
+        stopwatch.Start();
+        isChronometer = true;
+    }
 }
